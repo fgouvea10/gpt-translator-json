ad01d70e4cfb309f979d2769174cf989c8a2378b
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-05-09 21:06:11 -0300

    feat: create screen, add react-hook-form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,188 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+type LanguageType = "portuguese" | "english" | "spanish" | "french";
 
 export default function Home() {
+  const [inputLanguage, setInputLanguage] = useState<null | string>('portuguese');
+  const [outputLanguage, setOutputLanguage] = useState<null | string>('english');
+  const [inputValue, setInputValue] = useState('')
+
+  const { register, handleSubmit } = useForm()
+
+  console.log(inputValue)
+
+  const handleSelectInputLanguage = (language: LanguageType) => {
+    if (language !== outputLanguage) {
+      setInputLanguage(language);
+      return
+    }
+
+    return
+  };
+
+  const handleSelectOutputLanguage = (language: LanguageType) => {
+    if (language !== inputLanguage) {
+      setOutputLanguage(language);
+      return
+    }
+
+    return
+  };
+
+  const handleTranslateText = (data: any) => {
+    const gptInputText = `Translate only the values and not the keys of the JSON below to ${outputLanguage}`
+
+    console.log('GPT EXEPCTEC: ', `${gptInputText} ${data.inputValue}`)
+    console.log('input text', data.inputValue)
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex bg-zinc-900 min-h-screen overflow-y-auto lg:overflow-y-hidden">
+      <header className="w-full relative">
+        <div className="flex w-full max-w-7xl my-0 mx-auto items-center p-4">
+          <Link href="/" className="text-lg font-regular text-zinc-200">
+            GPT Translator JSON
+          </Link>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <main className="w-full relative">
+          <div className="flex flex-col w-full max-w-7xl my-12 mx-auto items-start p-4">
+            <div className="flex flex-col gap-1">
+              <h1 className="font-regular text-white text-3xl">
+                Create beautiful translations using AI.
+              </h1>
+              <p className="font-light text-zinc-400">
+                To get started, paste your JSON.
+              </p>
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <form className="w-full flex gap-12 items-start mt-12" onSubmit={handleSubmit(handleTranslateText)}>
+              <div className="w-1/2 flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
+                  <small className="text-zinc-400 font-light">From</small>
+                  <div className="flex items-center gap-4">
+                  <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        inputLanguage === "portuguese" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectInputLanguage("portuguese")}
+                    >
+                      Portuguese
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        inputLanguage === "english" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectInputLanguage("english")}
+                    >
+                      English
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        inputLanguage === "spanish" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectInputLanguage("spanish")}
+                    >
+                      Spanish
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        inputLanguage === "french" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectInputLanguage("french")}
+                    >
+                      French
+                    </button>
+                  </div>
+                </div>
+                <textarea
+                  className="p-4 bg-zinc-800 w-full h-96 rounded-lg border border-zinc-700 placeholder:text-zinc-500 placeholder:font-light text-zinc-300 font-light"
+                  placeholder="Paste here your json"
+                  {...register('inputValue')}
+                />
+                <div className="flex items-center gap-4">
+                  <button
+                    type="submit"
+                    className="bg-indigo-600 py-3 px-2 rounded cursor-pointer hover:bg-indigo-700 transition-colors text-white text-sm min-w-[120px]"
+                  >
+                    Translate
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-zinc-700 py-3 px-2 rounded cursor-pointer hover:bg-zinc-600 transition-colors text-white text-sm min-w-[120px]"
+                  >
+                    Clear
+                  </button>
+                </div>
+              </div>
+              <div className="w-1/2 flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
+                  <small className="text-zinc-400 font-light">To</small>
+                  <div className="flex items-center gap-4">
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        outputLanguage === "portuguese" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectOutputLanguage("portuguese")}
+                    >
+                      Portuguese
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        outputLanguage === "english" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectOutputLanguage("english")}
+                    >
+                      English
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        outputLanguage === "spanish" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectOutputLanguage("spanish")}
+                    >
+                      Spanish
+                    </button>
+                    <button
+                      type="button"
+                      className={`w-auto py-1 px-2 font-light text-zinc-300 hover:bg-zinc-800 transition-all rounded ${
+                        outputLanguage === "french" ? "bg-zinc-800" : ""
+                      }`}
+                      onClick={() => handleSelectOutputLanguage("french")}
+                    >
+                      French
+                    </button>
+                  </div>
+                </div>
+                <div className="p-4 bg-zinc-800 w-full h-96 rounded-lg border border-zinc-700 text-zinc-300 font-light" />
+                <div className="flex items-center gap-4">
+                  <button
+                    type="button"
+                    className="bg-indigo-600 py-3 px-2 rounded cursor-pointer hover:bg-indigo-700 transition-colors text-white text-sm min-w-[120px]"
+                  >
+                    Copy to clickboard
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-zinc-700 py-3 px-2 rounded cursor-pointer hover:bg-zinc-600 transition-colors text-white text-sm min-w-[120px]"
+                  >
+                    Reset
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </main>
+      </header>
+    </div>
+  );
 }
